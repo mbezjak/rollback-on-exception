6cbdeafa74ad5c85ff93633ae44f349121a694f4
Miro Bezjak <bezjak.miro@gmail.com>
2011-07-09 10:35:01 +0200

    Add test application in which rollback-on-exception isn't installed.

---
+++ test/app/plugin-not-installed/grails-app/conf/DataSource.groovy
@@ -0,0 +1,8 @@
+dataSource {
+    pooled = true
+    driverClassName = 'org.hsqldb.jdbcDriver'
+    dbCreate = 'create-drop'
+    url = 'jdbc:hsqldb:mem:test'
+    username = 'sa'
+    password = ''
+}
