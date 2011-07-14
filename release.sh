1585cf6b0a5de4f475a9e32eaa702aadf1509c19
Miro Bezjak <bezjak.miro@gmail.com>
2011-07-14 11:44:31 +0200

    Release script works with git.
    It will:
     * mention what is needed to do before releasing
     * do git tag
     * do git push
     * do git push --tags
     * release a plugin with automated commit message

--- release.sh
+++ release.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-grails release-plugin --zipOnly
+set -o errexit
+
+version="$1"
+[[ -z "$version" ]] && {
+  echo "Usage $(basename $0) VERSION" >&2
+  exit 1
+}
+
+cat <<EOF
+(1) make sure Changelog.md mentions version $version
+(2) update install section version in README.md
+(3) update version in plugin descriptor
+
+Press enter for plugin release, ctrl + c to cancel
+EOF
+read
+
+git tag "$version"
+git push
+git push --tags
+
+grails release-plugin --zipOnly --message="RELEASE $version"
+
+exit 0
